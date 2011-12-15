aae984a3a514ef7455972dfe61b72e8c9fb8339d
Iain Nash <info@codejoust.com>
2011-12-15 14:47:04 -0500

    Fixes for colors

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require 'sinatra'
 COLORS = {'black'=> 0, 'brown'=> 1, 'red'=> 2, 'orange'=> 3, 'yellow'=> 4, 'green'=> 5, 'blue'=> 6, 'violet'=> 7, 'grey'=> 8, 'white'=> 9}
 COLOR_VALS = {}
 MULTIPLIERS = {'silver'=> 0.01, 'gold'=> 0.1, 'black'=> 1, 'brown'=> 10, 'red'=> 100, 'orange'=> 1000, 'yellow'=> 10000, 'green'=> 100000, 'blue'=> 1000000, 'violet'=> 10000000}
-MULTIS = {-2 => 'silver', -1 => 'gold', 'black' => 0, 'brown' => 1, 'red' => 2, 'orange' => 3, 'yellow' => 4, 'green' => 5, 'blue' => 6, 'violet' => 7}
+MULTIS = {-2 => 'silver', -1 => 'gold', 0 => 'black', 1=> 'brown', 2=> 'red', 3=> 'orange', 4=>'yellow', 5=>'green', 6=>'blue', 7=>'violet'}
 
 COLORS.each_pair do |k,v|
   COLOR_VALS[v] = k
@@ -20,7 +20,7 @@ get '/color/:color' do |color|
 end
 
 get /([0-9]+)(\.[0-9]+)?(k|m|K|M)?/ do 
-  url_h = params[:captures] 
+  url_h = params[:captures]
   num = url_h[0].to_i
   num += url_h[1].to_f unless url_h[1].nil?
   num *= 1000 if (url_h[2] and url_h[2].downcase == 'k')
@@ -29,34 +29,13 @@ get /([0-9]+)(\.[0-9]+)?(k|m|K|M)?/ do
 end
 
 def num_2_color(num_in)
-  dec, val = calc_dec(num_in)
-  print "\n- dec: #{dec},val: #{val}\n"
-  val = val.to_s
-  digit_1 = 0; digit_2 = 0
-  if val.length == 1
-    digit_1 = val
-  else
-    digit_1 = val.to_s[0]
-    digit_2 = val.to_s[1]
-  end
-  [COLOR_VALS[digit_1], COLOR_VALS[digit_2], MULTIS[dec]]
+  dec, digit1, digit2 = calc_dec(num_in)
+  [COLOR_VALS[digit1], COLOR_VALS[digit2], MULTIS[dec]]
 end
 
 def calc_dec(num_in)
-  num_in = num_in
-  dec = 0
-  if (dec < 10)
-    while ((num_in * 10) % 10 != 0)
-      num_in *= 10
-      dec -= 1
-    end
-  else
-    while ((num_in % 10) == 0)
-      num_in /= 10
-      dec += 1
-    end
-  end
-  return [dec, num_in]
+  out = sprintf('%e',num_in).match(/([0-9])\.([0-9])[0-9]+e((\+|\-)[0-9]+)/)
+  return [out[3].to_i, out[1].to_i, out[2].to_i]
 end
 
 post '/' do
